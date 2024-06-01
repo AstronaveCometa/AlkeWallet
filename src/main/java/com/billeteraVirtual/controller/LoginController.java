b6829d8f0692520fffd7a5ebafe23a3e2707b4ee
AstronaveCometa <sebastian.leon.castillo@gmail.com>
2024-06-01 15:12:59 -0400

    1 commit

---
+++ src/main/java/com/billeteraVirtual/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.billeteraVirtual.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+	@GetMapping(value = {"/","/login"})
+	public String index(Model model) {
+		model.addAttribute("titulo", "Login");
+		return "login";
+	}
+}
